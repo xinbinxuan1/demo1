ec7176bd8c7cf3053653c795834f66c5736fad99
xinbinxuan1 <1239035840@qq.com>
2020-10-30 21:08:01 +0800

    提交

---
+++ commit/src/main/java/com/jk/entity/ReperBean.java
@@ -0,0 +1,18 @@
+package com.jk.entity;
+
+import lombok.Data;
+
+@Data
+public class ReperBean {
+    private Integer id;
+
+    private String shangname;
+
+    private String shangType;
+
+    private Integer huoNumber;
+
+    private String guiType;
+
+    private Integer kuNumber;
+}
